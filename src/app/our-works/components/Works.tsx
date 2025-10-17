e599a05760f506938f7fb8b6f1bb87ef4a6bff43
Princewill Jeremiah <princewilljeremiah83@gmail.com>
2025-10-17 17:12:11 +0100

    second commit

--- src/app/our-works/components/Works.tsx
+++ src/app/our-works/components/Works.tsx
@@ -1,6 +1,5 @@
 import ResponsiveHeader from '@/components/ResponsiveHeader'
 import React from 'react'
-
 import Image from 'next/image'
 import Link from 'next/link'
 import { Cards } from '@/components/constant'
